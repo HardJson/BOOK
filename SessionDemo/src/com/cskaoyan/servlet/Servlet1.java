9e73cbc91b98b1b907e51aad2992db60728b9e99
HardJson <tony1353855@163.com>
2019-07-31 23:49:47 +0800

    0731

---
+++ SessionDemo/src/com/cskaoyan/servlet/Servlet1.java
@@ -0,0 +1,26 @@
+package com.cskaoyan.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(name = "Servlet1",urlPatterns = "/test")
+public class Servlet1 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        HttpSession session = request.getSession(true);
+
+        String id = session.getId();
+
+        response.getWriter().println(id);
+
+    }
+}
